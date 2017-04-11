93b1a60cf9ca2fb04aa0f28de8b64cb938763592
Josh Klassen <Josh.Klassen@D2L.com>
2017-04-11 13:53:50 -0500

    Add alt text. Fire event upon failure to load image

--- test/d2l-image.js
+++ test/d2l-image.js
@@ -23,53 +23,72 @@ describe('<d2l-image>', function() {
 	});
 
 	describe('Hypermedia calls', function() {
-		it('should not send a request without an auth token', function() {
-			var sent = false;
+		describe('Sending requests', function() {
+			it('should not send a request without an auth token', function() {
+				var sent = false;
 
-			server.respondWith(
-				'GET',
-				widget.imageUrl,
-				function(req) {
-					sent = true;
-					req.respond(200);
-				});
+				server.respondWith(
+					'GET',
+					widget.imageUrl,
+					function(req) {
+						sent = true;
+						req.respond(200);
+					});
 
-			widget = fixture('d2l-image-fixture');
+				widget = fixture('d2l-image-fixture');
 
-			fakeTimer.tick(1);
-			expect(sent).to.equal(false);
+				fakeTimer.tick(1);
+				expect(sent).to.equal(false);
+			});
+
+			it('should send a request with an auth token', function() {
+				var sent = false;
+
+				server.respondWith(
+					'GET',
+					widget.imageUrl,
+					function(req) {
+						expect(req.requestHeaders['authorization']).to.equal('Bearer ' + 'My Token');
+						req.respond(200);
+						sent = true;
+					});
+
+				widget = fixture('d2l-image-fixture-populated');
+
+				fakeTimer.tick(1);
+				expect(sent).to.equal(true);
+			});
 		});
 
-		it('should send a request with an auth token', function() {
-			var sent = false;
+		describe('Receiving a response', function() {
+			it('should update image src attribute if the request is successful', function() {
+				var response = {
+					detail: {
+						response: new ArrayBuffer(8),
+						status: 200
+					}
+				};
 
-			server.respondWith(
-				'GET',
-				widget.imageUrl,
-				function(req) {
-					expect(req.requestHeaders['authorization']).to.equal('Bearer ' + 'My Token');
-					req.respond(200);
-					sent = true;
-				});
+				widget = fixture('d2l-image-fixture');
 
-			widget = fixture('d2l-image-fixture-populated');
+				widget._onImageResponse(response);
+				expect(widget.$.image.src);
+			});
 
-			fakeTimer.tick(1);
-			expect(sent).to.equal(true);
-		});
+			it('should fire a failed-to-load event if the request was not successful', function() {
+				var response = {
+					detail: {
+						status: 404
+					}
+				};
 
-		it('should update image src attribute', function() {
+				widget = fixture('d2l-image-fixture');
+				sinon.spy(widget, 'fire');
 
-			var response = {
-				detail: {
-					response: new ArrayBuffer(8)
-				}
-			};
-
-			widget = fixture('d2l-image-fixture');
-
-			widget._onImageResponse(response);
-			expect(widget.$.image.src);
+				widget._onImageResponse(response);
+				expect(!widget.$.image.src);
+				expect(widget.fire).to.have.been.calledWith('d2l-image-failed-to-load', response);
+			});
 		});
 	});
 });
